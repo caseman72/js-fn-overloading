7442188232291aa87704c92a0c655c0406925953
Casey Manion <casey@manion.com>
2013-07-23 14:20:13 -0700

    no 'fixes' just cleanup of for loops and using __proto__ instead of methods, explicit var declarations

--- src/overload.js
+++ src/overload.js
@@ -1,86 +1,83 @@
-;(function(){
+;(function() {
+  // this === window ~ normally
+	var ov = this.Overload = function(argsOps, func) {
+		argsOps = ov.convertArgsString(argsOps);
+		return function() {
+			return func.apply(this, ov.getCorrectArguments(arguments, argsOps));
+		}
+	};
 
-    var ov = this.Overload = function(argsOps, func) {
-        argsOps = ov.convertArgsString(argsOps);
-        return function(){
-            return func.apply(this, ov.getCorrectArguments(arguments, argsOps));
-        }
-    };
+	ov.__proto__ = {
+		convertArgsString: function(argsString) {
+			argsString = argsString.split(",");
 
-    ov.convertArgsString = function(argsString) {
-        argsString = argsString.split(',');
+			var reg = new RegExp("\\{([^}]+)\\}\\s*(\\[*\\w*)"); // fixes vim issues with brackets {}
+			// reg = /\{([^}]+)\}\s*(\[*\w*)/;
+			var args = {
+					length: argsString.length,
+					requiredLength: 0
+				};
 
-        var args = {
-                length: argsString.length,
-                requiredLength: 0
-            },
-            reg = /\{([^}]+)\}\s*(\[*\w*)/,
-            match, types, argument,
-            i, n;
+			for(var i = 0, n = argsString.length; i < n; i++) {
+				var match = argsString[i].match(reg);
+				var argument = {
+					types:    [],
+					mixed:    match[1] === "*",
+					required: match[2].charAt(0) !== "["
+				};
 
-        for(i = 0; i < argsString.length; i++) {
-            match = argsString[i].match(reg);
-            argument = {
-                types:    [],
-                mixed:    match[1] === '*',
-                required: match[2].charAt(0) !== '['
-            };
+				args[i] = argument;
 
-            args[i] = argument;
+				if(argument.required) args.requiredLength++;
 
-            if( argument.required ) args.requiredLength++;
+				if(argument.mixed) continue;
 
-            if( argument.mixed ) continue;
+				var types = match[1].split("|");
+				for(var j = 0, m = types.length; j < m; j++) {
+					argument.types.push(types[j].toLowerCase());
+				}
+			}
 
-            types = match[1].split('|');
-            for(n = 0; n < types.length; n++) {
-                argument.types.push(types[n].toLowerCase());
-            }
-        }
+			return args;
+		},
+		isValidArgument: function(argument, option) {
+			if(option.mixed) return true;
 
-        return args;
-    };
+			for(var i = 0, n = option.types.length; i < n; i++) {
+				if(typeof argument === option.types[i]) {
+					return true;
+				}
+			}
 
-    ov.isValidArgument = function(argument, option) {
-        if( option.mixed ) return true;
+			return false;
+		},
+		getCorrectArguments: function(args, ops) {
+			var correctArgs = new Array(ops.length);
 
-        for(var i = 0; i < option.types.length; i++) {
-            if( typeof argument === option.types[i] ) {
-                return true;
-            }
-        }
+			if(args.length === ops.length) {
+				for(var i = 0, n = args.length; i < n; i++) {
+					correctArgs[i] = args[i];
+				}
 
-        return false;
-    };
+				return correctArgs;
+			}
 
-    ov.getCorrectArguments = function(args, ops) {
-        var correctArgs = new Array(ops.length),
-            i;
+			var argsForRequired = args.length;
+			var required = ops.requiredLength;
 
-        if( args.length === ops.length ) {
-            for(i = 0; i < args.length; i++) {
-                correctArgs[i] = args[i];
-            }
+			for(var i = 0, j = 0, n = ops.length; i < n; i++) {
+				var option = ops[i];
+				var argument = args[j];
+				if(option.required || (argsForRequired - 1 >= required && ov.isValidArgument(argument, option))) {
+					correctArgs[i] = argument;
+					j++;
+					argsForRequired--;
+					if(option.required) required--;
+				}
+			}
 
-            return correctArgs;
-        }
-
-        var argsForRequired = args.length,
-            required = ops.requiredLength,
-            n, argument, option;
-
-        for(i = 0, n = 0; i < ops.length; i++) {
-            option = ops[i];
-            argument = args[n];
-            if( option.required || (argsForRequired - 1 >= required && ov.isValidArgument(argument, option)) ) {
-                correctArgs[i] = argument;
-                n++;
-                argsForRequired--;
-                if( option.required ) required--;
-            }
-        }
-
-        return correctArgs;
-    };
+			return correctArgs;
+		}
+	};
 
 }).call(this);
